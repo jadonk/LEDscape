fc5611735c62d1a72e3aefe379617e307c0f0e5d
Jason Kridner <jdk@ti.com>
2013-11-14 17:42:50 -0500

    Show rainbow 20 frames after color detection

--- colors.py
+++ colors.py
@@ -23,20 +23,29 @@ width = 32
 height = 16
 im = Image.new("RGB", (width,height), "black")
 im_draw = ImageDraw.Draw(im)
+im_pixels = im.load()
 capture = cv2.VideoCapture(-1)
 capture.set(cv.CV_CAP_PROP_FRAME_WIDTH, 160)
 capture.set(cv.CV_CAP_PROP_FRAME_HEIGHT, 100)
 last_color = "Green"
 
 def rainbow(i):
-	rgb = [int(x*256) for x in hsv_to_rgb(i/256.0,0.8,0.8)]
+	rgb = [int(x*256) for x in hsv_to_rgb((i%256)/256.0,0.8,0.8)]
 	return (rgb[0],rgb[1],rgb[2])
 
 def myfill(i):
 	if i < 100:
 		return (0, 0, 0)
-	rgb = [int(x*256) for x in hsv_to_rgb((i%256)/256.0,0.8,0.8)]
-	return (rgb[0],rgb[1],rgb[2])
+	return rainbow(i)
+
+rainbowColors = [rainbow((i/180.0)*256.0) for i in range(0, 180)]
+
+def r_array(cycle):
+	color = cycle % 180
+	for x in range(0, width):
+		for y in range(0, height):
+			index = int(color + x + y*10/4) % 180
+			im_pixels[x, y] = rainbowColors[index];
 
 def getColor():
 	(status, cam) = capture.read()
@@ -58,8 +67,11 @@ while True:
 		i = 0
 		last_color = cname
 		#print cname
-	im.paste(rainbow(color), (0,0,width,height))
-	im_draw.text((2, 0), cname, font=font_sm, fill=myfill(i))
+	if i < 20:
+		im.paste(rainbow(color), (0,0,width,height))
+		im_draw.text((2, 0), cname, font=font_sm, fill=(0,0,0))
+	else:
+		r_array(i)
 	sock.sendto(chr(1) + im.tostring(), dest)
 	i += 1
 	if i > 65536:
